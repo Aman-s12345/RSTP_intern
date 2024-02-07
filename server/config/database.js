e284f3cbf44a11ad285791906db7c9a8dbe9c9d3
aman <114105332+Aman-s12345@users.noreply.github.com>
2024-02-07 18:58:03 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,8 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect("mongodb+srv://amansinghbiuri:amanhaha@cluster0.bkvvfnl.mongodb.net/?retryWrites=true&w=majority/Mediaplayer", {
+		.connect("mongodb+srv://soulopscout:sfaman23@cluster0.druj2gv.mongodb.net/?retryWrites=true&w=majority/Mediaplayer", {
+			
 			
 			
 		})
