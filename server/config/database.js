b94b4dc05f0fa585442390abf04272a7fd66cbc3
aman <114105332+Aman-s12345@users.noreply.github.com>
2024-02-07 18:39:57 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,8 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect("mongodb://amansinghbiuri:amanhaha@cluster0.bkvvfnl.mongodb.net?retryWrites=true&w=majority/Mediaplayer", {
+		.connect("mongodb+srv://amansinghbiuri:amanhaha@cluster0.bkvvfnl.mongodb.net/?retryWrites=true&w=majority/Mediaplayer", {
+			
 			
 		})
 		.then(console.log(`DB ka Connection Success`))
