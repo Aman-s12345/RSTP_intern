cd9eaf0f164f771b4b17d96b8c9b4c83438c8add
aman <114105332+Aman-s12345@users.noreply.github.com>
2024-02-07 18:15:35 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,9 +5,8 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect("mongodb://amansinghbiuri:amanhaha@cluster0.bkvvfnl.mongodb.net/Mediaplayer", {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
+		.connect("mongodb://amansinghbiuri:amanhaha@cluster0.bkvvfnl.mongodb.net?retryWrites=true&w=majority/Mediaplayer", {
+			
 		})
 		.then(console.log(`DB ka Connection Success`))
 		.catch((err) => {
